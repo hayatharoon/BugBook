ceffc74d0c774abb0dd49fee36170ed492699ac9
Haroon Hayat <hayat_haroon@yahoo.com>
2024-08-18 19:27:07 +0500

    write validation for login and signup

---
+++ src/auth.ts
@@ -0,0 +1,79 @@
+import { PrismaAdapter } from "@lucia-auth/adapter-prisma";
+import prisma from "./lib/prisma";
+import { Lucia, Session, User } from "lucia";
+import { cache } from "react";
+import { cookies } from "next/headers";
+
+const adapter = new PrismaAdapter(prisma.session, prisma.user);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: {
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
+  getUserAttributes(attributes) {
+    return {
+      id: attributes.id,
+      username: attributes.username,
+      displayName: attributes.displayName,
+      avatarUrl: attributes.avatarUrl,
+      googleId: attributes.googleId,
+    };
+  },
+});
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  id: string;
+  username: string;
+  displayName: string;
+  avatarUrl: string | null;
+  googleId: string | null;
+}
+
+// ? cache function is React function which allow us to cache the data. for example we make a request to server for some data and we need that data somewhere else then we can user the cache function overthere to cache the data and don't make the multiple server request.
+export const validateRequest = cache(
+  async (): Promise<
+    { user: User; session: Session } | { user: null; session: null }
+  > => {
+    const sessionId = cookies().get(lucia.sessionCookieName)?.value ?? null;
+    if (!sessionId) {
+      return {
+        user: null,
+        session: null,
+      };
+    }
+
+    const result = await lucia.validateSession(sessionId);
+
+    try {
+      if (result.session && result.session.fresh) {
+        const sessionCookie = lucia.createSessionCookie(result.session.id);
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+
+      if (!result.session) {
+        const sessionCookie = lucia.createBlankSessionCookie();
+        cookies().set(
+          sessionCookie.name,
+          sessionCookie.value,
+          sessionCookie.attributes,
+        );
+      }
+    } catch (error) {}
+
+    return result;
+  },
+);
