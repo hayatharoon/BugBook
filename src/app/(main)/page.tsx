deb97b41ea7526dd4ac4cb038ef8a2a86bd84ca5
Haroon Hayat <hayat_haroon@yahoo.com>
2024-09-04 00:26:32 +0500

    trending topic, unstable_cache and suspense

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import Post from "@/components/posts/editor/Post";
 import PostEditor from "@/components/posts/editor/PostEditor";
+import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/type";
 
@@ -9,13 +10,14 @@ export default async function Home() {
     orderBy: { createAt: "desc" },
   });
   return (
-    <main className="w-full min-w-0">
+    <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Post post={post} key={post.id} />
         ))}
       </div>
+      <TrendsSidebar />
     </main>
   );
 }
